4500051e19228509e32c884b957334cc75e8e230
stayup24h <102606991+stayup24h@users.noreply.github.com>
2024-11-07 02:06:36 +0900

    title ui 구현

---
+++ Assets/Scripts/UI Scripts/Stage Popup Start Script.cs
@@ -0,0 +1,14 @@
+using System.Collections;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.EventSystems;
+
+public class StagePopupStartScript : MonoBehaviour
+, IPointerClickHandler
+
+{
+    public void OnPointerClick(PointerEventData eventData)
+    {
+        //Stage POPup Start
+    }
+}
