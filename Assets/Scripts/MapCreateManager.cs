062e545c985e596be7ac4907685ebf83541caed0
villainouskirby <69393156+villainouskirby@users.noreply.github.com>
2024-11-06 18:35:49 +0900

    맵 삭제 알고리즘 추가

--- Assets/Scripts/MapCreateManager.cs
+++ Assets/Scripts/MapCreateManager.cs
@@ -1,9 +1,13 @@
+using System.Collections.Generic;
 using UnityEngine;
 
 public class MapCreateManager : MonoBehaviour
 {
     [SerializeField] private float distance;
     [SerializeField] private GameObject patternPrefab;
+    [SerializeField] private int maxPatternCnt;
+
+    private Queue<GameObject> patternQueue = new Queue<GameObject>();
 
     private Vector3 endPos = Vector3.zero;
 
@@ -11,10 +15,17 @@ public class MapCreateManager : MonoBehaviour
     {
         if (Input.GetKeyDown(KeyCode.Space))
         {
-            float degree = Random.value * 60;
-            GameObject go = Instantiate(patternPrefab);
-            go.transform.position = endPos + new Vector3(Mathf.Cos(degree * Mathf.Deg2Rad), Mathf.Sin(degree * Mathf.Deg2Rad), 0) * distance * Random.value;
-            endPos = go.transform.Find("EndPos").position;
+            CreateMap();
         }
+    }
+
+    public void CreateMap()
+    {
+        float degree = Random.value * 60;
+        GameObject go = Instantiate(patternPrefab);
+        go.transform.position = endPos + new Vector3(Mathf.Cos(degree * Mathf.Deg2Rad), Mathf.Sin(degree * Mathf.Deg2Rad), 0) * distance * Random.value;
+        endPos = go.transform.Find("EndPos").position;
+        patternQueue.Enqueue(go);
+        if (patternQueue.Count > maxPatternCnt) Destroy(patternQueue.Dequeue());
     }
 }
