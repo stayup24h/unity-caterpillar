bf4b824693c26241c2269f30eb4e47a1fbed6749
villainouskirby <69393156+villainouskirby@users.noreply.github.com>
2024-11-07 21:56:17 +0900

    remix mode 스코어 텍스트 수정

--- Assets/Scripts/grass scripts/NewMonoBehaviourScript.cs
+++ Assets/Scripts/grass scripts/NewMonoBehaviourScript.cs
@@ -3,14 +3,15 @@ using UnityEngine;
 public class NewMonoBehaviourScript : MonoBehaviour
 {
     // Start is called once before the first execution of Update after the MonoBehaviour is created
-    void Start()
-    {
-        
-    }
-
+    private float moveSpeed = 3f;
     // Update is called once per frame
     void Update()
     {
-        
+        transform.position += Vector3.down * moveSpeed * Time.deltaTime;
+        if (transform.position.y < -10)
+        {
+            transform.position += new Vector3(0, 20f,0);
+        }
     }
 }
+
