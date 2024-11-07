e748ae27b85b64e020e538587460a6798d84c9bf
villainouskirby <69393156+villainouskirby@users.noreply.github.com>
2024-11-07 22:01:33 +0900

    merge conflict수정

--- Assets/Scripts/grass scripts/NewMonoBehaviourScript.cs
+++ Assets/Scripts/grass scripts/NewMonoBehaviourScript.cs
@@ -3,15 +3,14 @@ using UnityEngine;
 public class NewMonoBehaviourScript : MonoBehaviour
 {
     // Start is called once before the first execution of Update after the MonoBehaviour is created
-    private float moveSpeed = 3f;
+    void Start()
+    {
+        
+    }
+
     // Update is called once per frame
     void Update()
     {
-        transform.position += Vector3.down * moveSpeed * Time.deltaTime;
-        if (transform.position.y < -10)
-        {
-            transform.position += new Vector3(0, 20f,0);
-        }
+        
     }
 }
-
