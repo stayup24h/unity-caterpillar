888e981bbd8753c33a8ee1c57e1176beb8439dee
villainouskirby <69393156+villainouskirby@users.noreply.github.com>
2024-11-08 02:01:36 +0900

    기믹 수정

--- Assets/Scripts/Gimmick Scripts/BlinkPlatform.cs
+++ Assets/Scripts/Gimmick Scripts/BlinkPlatform.cs
@@ -2,43 +2,30 @@ using UnityEngine;
 
 public class BlinkPlatform : MonoBehaviour
 {
-    private CaterpillarCtrl caterpillarCtrl;
+    private Transform caterpillar;
+    private Head_Tail head;
+    private Head_Tail tail;
     private Rigidbody2D head_rb;
     private Rigidbody2D tail_rb;
-    private bool isAttach;
 
-    void Start()
+    void Awake()
     {
-        caterpillarCtrl = FindObjectsByType<CaterpillarCtrl>(FindObjectsSortMode.None)[0];
-        head_rb = caterpillarCtrl.transform.GetChild(0).GetComponent<Rigidbody2D>();
-        tail_rb = caterpillarCtrl.transform.GetChild(6).GetComponent<Rigidbody2D>();
+        caterpillar = GameObject.Find("Caterpillar").transform;
+        head = caterpillar.GetChild(0).GetComponent<Head_Tail>();
+        tail = caterpillar.GetChild(6).GetComponent<Head_Tail>();
+        head_rb = head.GetComponent<Rigidbody2D>();
+        tail_rb = tail.GetComponent<Rigidbody2D>();
     }
 
     public void PlatformDisappear()
     {
-        if (isAttach)
+        if (head != null && ReferenceEquals(head.attachedObject, gameObject))
         {
             head_rb.constraints = RigidbodyConstraints2D.None;
-            tail_rb.constraints = RigidbodyConstraints2D.None;
-            isAttach = false;
         }
-    }
-
-    private void OnCollisionEnter2D(Collision2D collision)
-    {
-        if (collision.gameObject.CompareTag("Player"))
-            isAttach = true;
-    }
-
-    private void OnCollisionStay2D(Collision2D collision)
-    {
-        if (collision.gameObject.CompareTag("Player"))
-            isAttach = true;
-    }
-
-    private void OnCollisionExit2D(Collision2D collision)
-    {
-        if (collision.gameObject.CompareTag("Player"))
-            isAttach = false;
+        if (tail != null && ReferenceEquals(tail.attachedObject, gameObject))
+        {
+            tail_rb.constraints = RigidbodyConstraints2D.None;
+        }
     }
 }
