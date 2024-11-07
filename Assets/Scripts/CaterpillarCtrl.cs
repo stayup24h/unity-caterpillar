68a994901d6b51adc021c661cc84cfc26d46a49e
mingi <chamingy1@gmail.com>
2024-11-07 22:18:34 +0900

    기어가는 소리 한번만 나게

--- Assets/Scripts/CaterpillarCtrl.cs
+++ Assets/Scripts/CaterpillarCtrl.cs
@@ -78,13 +78,11 @@ public class CaterpillarCtrl : MonoBehaviour
         {
             //MoveHead_JoyStick();
             MoveHead_KeyBoard();
-            soundCtrl.StartMoveSound();
         }
         else
         {
             //MoveTail_JoyStick();
             MoveTail_KeyBoard();
-            soundCtrl.StartMoveSound();
         }
     }
 
@@ -144,6 +142,7 @@ public class CaterpillarCtrl : MonoBehaviour
     {
         state = State.none;
         isHeadTurn = !isHeadTurn;
+        soundCtrl.StartMoveSound();
     }
 
     void MoveHead_KeyBoard()
